1b9be1944d61bd75f4b2ae549b6a65d57f3823fb
InfluencerGhost <eokorie1911@gmail.com>
2025-09-08 03:22:19 +0100

    update random number generator to produce psuedo random number from 1 - 10

--- randomnumber generator/index.js
+++ randomnumber generator/index.js
@@ -2,6 +2,6 @@ const generate = document.getElementById('generate');
 const number = document.getElementById('number');
 
 generate.onclick = function() {
-    let randomNumber = Math.random()
+    let randomNumber = Math.floor(Math.random() * 10) + 1
     number.textContent = randomNumber;
 }
