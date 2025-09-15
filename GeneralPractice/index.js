98e130e8169841b78e073fd10f45a38d79531523
InfluencerGhost <eokorie1911@gmail.com>
2025-09-15 14:58:44 +0100

    Tutorials Added

--- GeneralPractice/index.js
+++
@@ -1,7 +0,0 @@
-numbers = [1, 2, 3, 4, 5]
-
-numbers.forEach(display)
-
-function display (element) {
-    console.log(element)
-}
---
+++ GeneralPractice/tutorials/constructor.js
@@ -0,0 +1,17 @@
+function Car (make, model, year, color) {
+this.make = make
+this.model = model
+this.year = year 
+this.color = color
+this.drive = function() {console.log(`You drive the ${this.model} ${this.year} model`)
+}
+this.specs = function() {
+    console.log(`You picked a ${this.make} ${this.model} manufactured in ${this.year} colored ${this.color}`)
+}
+}
+
+const car1 = new Car ("Mercedes", "GLE", "2025", "Black")
+
+console.log(car1)
+car1.specs()
+car1.drive()
