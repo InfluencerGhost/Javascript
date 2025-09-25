870cec2f9a3f7904842db5dcd8f256170f0cd47a
InfluencerGhost <eokorie1911@gmail.com>
2025-09-25 17:33:05 +0100

    tutorials

--- GeneralPractice/index.js
+++ GeneralPractice/index.js
@@ -1,21 +1,57 @@
-class Rectangle {
-    constructor (width, height) {
-        this.width
-        this.height
+class Person {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
     }
 
-    set width(newWidth) {
-        if (newWidth < 0) {
-            this._width = newWidth
+    set firstName(newFirstName) {
+
+        if (typeof newFirstName === "string" && newFirstName.length > 1) {
+            this._firstName = newFirstName
         } else {
-            console.error("Please input a valid number")
+            console.error("First name nmust be a non empty string")
         }
     }
 
+        set lastName(newLastName) {
+
+        if (typeof newLastName === "string" && newLastName.length > 1) {
+            this._lastName = newLastName
+        } else {
+            console.error("Last name nmust be a non empty string")
+        }
+    }
+
+        set age(newAge) {
+
+        if (newAge > 1) {
+            this._age = newAge
+        } else {
+            console.error("age must be a number greater than 0")
+        }
+    }
+
+    get firstName() {
+        return this._firstName
+    }
+
+    get lastName() {
+        return this._lastName
+    }
+
+    get age() {
+        return this._age
+    }
+
+    get fullName() {
+        return `${this._firstName} ${this._lastName}`
+    }
 }
 
+const person = new Person ("mary", "jane", 23)
 
-const rectangle = new Rectangle(3, 4)
-
-console.log(rectangle.width)
-console.log(rectangle.height)
+console.log(person.firstName)
+console.log(person.lastName)
+console.log(person.age)
+console.log(person.fullName)
