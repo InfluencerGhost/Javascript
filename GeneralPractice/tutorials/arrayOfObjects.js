9ea462a7787c2899077b47c906417765a549e818
InfluencerGhost <eokorie1911@gmail.com>
2025-09-26 13:07:16 +0100

    array of objects updated

--- GeneralPractice/tutorials/arrayOfObjects.js
+++ GeneralPractice/tutorials/arrayOfObjects.js
@@ -12,7 +12,7 @@ const food = [
 ]
 
 
-function displayFruit(number) {
+function displayFood(number) {
     if (number < 0) {
         console.log("enter a valid number >= 0")
     } else if (food[number].color.length === 1) {
@@ -21,9 +21,14 @@ function displayFruit(number) {
          console.log(` THe ${food[number].name} is a ${food[number].type} and it has colors ${food[number].color[0]} and ${food[number].color[1]}`)
     }
 }
+function addFood(name, type, color) {
+    food.push({name: name, type: type, color: [color],})
+}
 
-displayFruit(1)
-displayFruit(0)
+displayFood(1)
+displayFood(0)
+addFood("pineapple", "fruit", "yellow")
+displayFood(2)
 
 
 
