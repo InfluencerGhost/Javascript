3b2a8ad647995cce6a9104be1aba2a528a5ba2aa
InfluencerGhost <eokorie1911@gmail.com>
2025-09-26 10:15:27 +0100

    nested Objects

--- GeneralPractice/tutorials/destructuring.js
+++ GeneralPractice/tutorials/destructuring.js
@@ -11,3 +11,7 @@ const colours = ["red", "green", "blue", "white", "orange"];
 [colours[0], colours[4]] = [colours[4], colours[0]];
 
 console.log(colours);
+
+const [firstColour, secondColour, thirdColour, ...extraColours] = colours;
+
+console.log(extraColours);
