c596c1ace0e2b9877174696ac5b1edf538f544af
InfluencerGhost <eokorie1911@gmail.com>
2025-09-26 12:53:31 +0100

    array of objects

--- GeneralPractice/index.js
+++ GeneralPractice/index.js
@@ -1,20 +1,23 @@
-class Person {
-    constructor(name, age, ...address)  {
-        this.name = name
-        this.age = age
-        this.address = new Address(...address)
+const food = [
+    {
+        name: "orange",
+        type: "fruit",
+        color: "orange",
+    },
+    {
+        name: "carrot",
+        type: "vegetable",
+        color: ["green", "orange"],
+    },
+]
+
+
+function displayFruit(number) {
+    if (number < 0) {
+        console.log("enter a valid number >= 0")
+    } else if (food[number].color.length === 1) {
+    console.log(`${food[number].name} is a ${food[number].name} and its color ${food[number].color}`)
+    } else {
+         console.log(`${food[number].name} is a ${food[number].name} and its color ${food[number].color[1]} and ${food[number].color[1]}`)
     }
 }
-
-class Address {
-    constructor(street, city, country) {
-        this.steeet = street
-        this.city = city
-        this.country = country
-    }  
-}
-
-const person1 = new Person("spongebob", 30, "124 cont street", "bikin bottom", "int. waters" )
-
-
-console.log(person1.address)
---
+++ GeneralPractice/tutorials/arrayOfObjects.js
@@ -0,0 +1,14 @@
+const food =[
+    {
+        name: "orange",
+        type: "fruit",
+        color: "orange"
+    },
+    {
+        name: "carrot",
+        type: "vegetable",
+        color: ["green", "orange"]
+    }
+]
+
+console.log(food[1])
