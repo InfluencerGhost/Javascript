102223a53f9e66d39d855949a807d70eeb9580ab
InfluencerGhost <eokorie1911@gmail.com>
2025-09-26 12:33:15 +0100

    nested Objects update

--- GeneralPractice/index.js
+++ GeneralPractice/index.js
@@ -1,12 +1,20 @@
-const person = {
-    fullName: "Emmanuel",
-    age: 17,
-    school: {
-        name: "FUTO",
-        School: "SICT",
-        department: "SOE",
-        yearsofStudy: 5,
-    },
+class Person {
+    constructor(name, age, ...address)  {
+        this.name = name
+        this.age = age
+        this.address = new Address(...address)
+    }
 }
 
-console.log(person)
+class Address {
+    constructor(street, city, country) {
+        this.steeet = street
+        this.city = city
+        this.country = country
+    }  
+}
+
+const person1 = new Person("spongebob", 30, "124 cont street", "bikin bottom", "int. waters" )
+
+
+console.log(person1.address)
--- GeneralPractice/tutorials/nestedObjects.js
+++ GeneralPractice/tutorials/nestedObjects.js
@@ -10,3 +10,25 @@ const person = {
 }
 
 console.log(person)
+
+
+class Person {
+    constructor(name, age, ...address)  {
+        this.name = name
+        this.age = age
+        this.address = new Address(...address)
+    }
+}
+
+class Address {
+    constructor(street, city, country) {
+        this.steeet = street
+        this.city = city
+        this.country = country
+    }  
+}
+
+const person1 = new Person("spongebob", 30, "124 cont street", "bikin bottom", "int. waters" )
+
+
+console.log(person1.address)
