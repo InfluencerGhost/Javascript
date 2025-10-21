34347a5c826ac6decc7c08bfb35d23ee17f45e52
InfluencerGhost <eokorie1911@gmail.com>
2025-10-21 15:06:29 +0100

    GEneral Practice

--- GeneralPractice/tutorials/errorHandling.js
+++ GeneralPractice/tutorials/errorHandling.js
@@ -1,14 +1,24 @@
+const button = document.getElementById('aButton')
+const errorText = document.getElementById('errorText')
+button.onclick = function start() {
 
 try {
 const dividend = Number(window.prompt("what is the dividend: "));
 const divisor = Number(window.prompt("what is the divisor: "));
+
 const result = dividend/divisor;
+console.log(result)
 
 if (divisor == 0) {
     throw new Error("Division by zero is impossible");
 }
+
+if(isNaN(dividend) || isNaN(divisor)) {
+    throw new Error("Please input a number")
+}
 } catch(error) {
-console.error(error);
+    errorText.textContent = error;
 } finally {
     console.log("The program has ended!");
+}
 }
