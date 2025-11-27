703ce2ddc6b37bb93a15ba322dc601bf31449acd
InfluencerGhost <eokorie1911@gmail.com>
2025-11-27 14:10:43 +0100

    getting tarted with fetch

--- GeneralPractice/index.js
+++ GeneralPractice/index.js
@@ -1,4 +1,31 @@
-fetch("people.json")
-    .then(response => response.json())
-    .then(values => console.log(values[0].name))
-    .catch(error => console.error(error))
+// fetch("people.json")
+//     .then(response => response.json())
+//     .then(values => console.log(values[0].name))
+//     .catch(error => console.error(error))
+
+function punchMan() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("i punched man")
+        }, 5000)
+
+    })
+}
+
+function punchWoman() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("i punched woman")
+        }, 500)
+
+    })
+}
+
+async function fight () {
+    punchedMan = await punchMan();
+    console.log(punchedMan)
+    punchedWoman = await punchWoman();
+    console.log(punchedWoman)
+}
+
+fight();
